206c0e68911a73f353ce2b30aee93280d333864b
alvinVMWare <alvinchristianto93@gmail.com>
2019-03-18 20:57:17 +0700

    edit clock.py #1

--- clock.py
+++ clock.py
@@ -8,8 +8,8 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', day_of_week='mon-sun', hour = 12)
-def timed_job():
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour = 12)
+def scheduled_job():
     #print('This job is run every three minutes.')
    
     # Twitter authentication
